507922402564eb74b0abc135cecae6e9278cfae6
Ivan Nikolov <ivannikolov007@gmail.com>
2017-11-12 17:15:38 +0200

    Setup vagrant

--- program/test.py
+++ program/test.py
@@ -1,4 +1,7 @@
 import subprocess 
+import os
+from subprocess import call
+
 data = subprocess.check_output(['dpkg', '-l']);
 
 data = data.splitlines();
@@ -7,4 +10,20 @@ del data[0:5]
 
 for line in data:
 	line = line.split()
-	print line[1] , line[2]
+	line = line[1] + '=' + line[2]
+	print line
+
+directory = os.getcwd()
+directory = directory + "/vagrant"
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+os.chdir(directory);
+
+print os.getcwd()
+
+call(["vagrant","init","ubuntu/xenial64"])
+call(["vagrant","up"])
+call(["vagrant","ssh"])
+call(["touch","test.txt"])
